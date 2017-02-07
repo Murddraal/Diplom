ebbbc614e1631b6583831bca1f862eb0679f90b8
Murddraal <2dents@mail.ru>
2017-02-07 22:22:55 +0700

    переход на другой компьютер

--- translator.py
+++ translator.py
@@ -3,6 +3,7 @@ import json
 import csv
 import re
 from docx import Document
+from docx.shared import Pt
 # coding: utf8
 
 
@@ -11,6 +12,7 @@ class translator(object):
         self.url = 'https://translate.yandex.net/api/v1.5/tr.json/translate?'
         self.key = 'trnsl.1.1.20170127T090047Z.d0549b75a2237806.40d7e9d8902cb56bc4115af8e88c1bf8f0e4cbf3'
         self.lang = lang
+        self.max_text_size = 10000
 
     def csv_reading(self, csv_filename):
         with open(csv_filename, encoding='utf-8') as csvfile:
@@ -27,6 +29,9 @@ class translator(object):
         except:
             document = Document()
 
+        run = document.add_paragraph().add_run()
+        font = run.font
+        font.name, font.size = 'Times New Roman', Pt(6)
         table = document.add_table(rows=1, cols=2)
         row = table.rows[0]
         row.cells[0].text, row.cells[1].text = orig_text, trans_text
@@ -39,12 +44,14 @@ class translator(object):
 
     def translating(self, text):
 
-        splited_text = re.split(r'[?!\.]', text)
+        splited_text = re.split(r'[?!\n\.]', text)
 
         translated = ""
         for i in splited_text:
-            if len(i) > 10000:
+            if len(i) > self.max_text_size:
                 return translated
+            if len(re.findall(r'[^\s?1\n]', i)) == 0:
+                continue
             translation = requests.post(self.url,
                                         data={'key': self.key,
                                               'text': i,
@@ -52,5 +59,5 @@ class translator(object):
                                         )
 
             jtr = json.loads(translation.text)
-            translated += jtr['text'][0] + " "
+            translated += jtr['text'][0] + " ||| "
         return translated
