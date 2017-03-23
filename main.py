9f929d474ec741aadeb56f11e51fd0c6ef905504
Murddraal <2dents@mail.ru>
2017-03-23 23:19:48 +0700

    word clouds

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
-import translator as tr
 import operator
 import time
+import translator as tr
+from wordcloud import WordCloud
+from os import path
+import matplotlib.pyplot as plt
 # coding: utf8
 
-
 def main():
-
-
-    test = tr.translator('ru')
+    test = tr.translator('en')
     x = test.csv_reading('emails.csv')
     k = 1
     start_time = time.time()
@@ -15,14 +15,34 @@ def main():
         #print(k)
         k += 1
         text = test.text_parsing(i)
+        if text != "":
+            wordcloud = WordCloud().generate(text)
+            plt.imshow(wordcloud, interpolation = 'bilinear')
+            plt.axis("off")
+            wordcloud = WordCloud(max_font_size=40).generate(text)
+            plt.figure()
+            plt.imshow(wordcloud, interpolation="bilinear")
+            plt.axis("off")
+            url = './clouds/cloud{}.png'.format(k)
+            plt.savefig(url)
+
+            words = wordcloud.words_
+            sorted_words = sorted(words.items(), key = operator.itemgetter(1))
+            sorted_words.reverse()
+            with open('./clouds/cloud{}.txt'.format(k), 'w') as f:
+                for j in sorted_words:
+                    f.write('{} - {}\n'.format(j[0], j[1]))
+
         # tr_text = test.translating(text)
         # test.writing(text, tr_text)
         # print("wr")
-        langs = test.lang_detecting(text, 1)
+        langs = test.lang_detecting(text, 0)
+        if k == 5:
+            break
     total_time = time.time() - start_time
 
     sorted_langs = sorted(langs.items(), key=operator.itemgetter(1), reverse=False)
-    
+
     sorted_langs.reverse()
     with open('langs_ya.txt', 'w') as f:
         f.write('Time: ' + str(total_time) + '\n\n')
